b0e1b8773454151cd488a87710b7c56c6dc26e0b
boedegoat <bhremada.fka@gmail.com>
2022-03-09 08:54:38 +0700

    Make crew page responsive for tablet

--- pages/crew.tsx
+++ pages/crew.tsx
@@ -16,20 +16,17 @@ const Crew: NextPage<ICrew> = ({ crewTab }) => {
   console.log(crewData)
 
   return (
-    <Container
-      screenClassName="bg-crew-mobile tablet:bg-crew-tablet"
-      className="wrapper"
-    >
+    <Container screenClassName="bg-crew-mobile tablet:bg-crew-tablet">
       <Heading2 number={2}>MEET YOUR CREW</Heading2>
-      <div className="mt-8 space-y-8 pb-8">
+      <div className="wrapper mt-8 flex flex-col space-y-8 pb-8 tablet:px-14 tablet:pb-0">
         {/* slider */}
-        <div>
+        <div className="tablet:order-2">
           <img
             src={formatPath(crewData.images.webp)}
             alt={crewData.name}
-            className="h-[223px] w-[327px] border-b-2 border-gray object-contain"
+            className="mx-auto h-[223px] w-[327px] border-b-2 border-gray object-contain tablet:h-[532px] tablet:w-auto tablet:border-b-0"
           />
-          <div className="mt-8 flex justify-center space-x-4">
+          <div className="mt-8 flex justify-center space-x-4 tablet:hidden">
             {crewNames.map((crewName, index) => (
               <Link
                 href={{ query: { crewTab: crewName } }}
@@ -43,18 +40,30 @@ const Crew: NextPage<ICrew> = ({ crewTab }) => {
             ))}
           </div>
         </div>
-        <div className="text-center">
-          <h3 className="font-bellefair uppercase text-white opacity-[49.51%]">
+        <div className="text-center tablet:order-1">
+          <h3 className="font-bellefair uppercase text-white opacity-[49.51%] tablet:text-2xl">
             {crewData.role}
           </h3>
-          <h1 className="mt-2 font-bellefair text-2xl uppercase text-white">
+          <h1 className="mt-2 font-bellefair text-2xl uppercase text-white tablet:mt-3 tablet:text-[40px]">
             {crewData.name}
           </h1>
-          <p className="mt-4 text-[15px] leading-[25px] text-blue-100">
+          <p className="mt-4 text-[15px] leading-[25px] text-blue-100 tablet:text-base tablet:leading-[28px]">
             {crewData.bio}
           </p>
+          <div className="my-10 hidden justify-center space-x-4 tablet:flex ">
+            {crewNames.map((crewName, index) => (
+              <Link
+                href={{ query: { crewTab: crewName } }}
+                key={index}
+                scroll={false}
+                className={cn(
+                  'inline-block h-[10px] w-[10px] rounded-full bg-white transition-opacity hover:opacity-50',
+                  crewTab == crewName ? 'opacity-1' : 'opacity-[17.44%]'
+                )}
+              ></Link>
+            ))}
+          </div>
         </div>
-        {/* informations */}
       </div>
     </Container>
   )
