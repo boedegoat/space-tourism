0bde32204b6d37902e227b1abe5235b252404ea3
boedegoat <bhremada.fka@gmail.com>
2022-03-11 08:36:21 +0700

    Make crew page responsive for desktop

--- pages/crew.tsx
+++ pages/crew.tsx
@@ -16,48 +16,49 @@ const Crew: NextPage<ICrew> = ({ crewTab }) => {
   console.log(crewData)
 
   return (
-    <Container screenClassName="bg-crew-mobile tablet:bg-crew-tablet">
+    <Container
+      screenClassName="bg-crew-mobile tablet:bg-crew-tablet desktop:bg-crew-desktop flex flex-col"
+      className="desktop:wrapper desktop:flex desktop:flex-grow desktop:flex-col"
+    >
       <Heading2 number={2}>MEET YOUR CREW</Heading2>
-      <div className="wrapper mt-8 flex flex-col space-y-8 pb-8 tablet:px-14 tablet:pb-0">
+      <div className="wrapper mt-8 flex flex-grow flex-col space-y-8 pb-8 tablet:px-14 tablet:pb-0 desktop:mt-0 desktop:flex-row desktop:items-stretch desktop:px-0">
         {/* slider */}
-        <div className="tablet:order-2">
-          <img
-            src={formatPath(crewData.images.webp)}
-            alt={crewData.name}
-            className="mx-auto h-[223px] w-[327px] border-b-2 border-gray object-contain tablet:h-[532px] tablet:w-auto tablet:border-b-0"
-          />
-          <div className="mt-8 flex justify-center space-x-4 tablet:hidden">
-            {crewNames.map((crewName, index) => (
-              <Link
-                href={{ query: { crewTab: crewName } }}
-                key={index}
-                scroll={false}
-                className={cn(
-                  'inline-block h-[10px] w-[10px] rounded-full bg-white transition-opacity hover:opacity-50',
-                  crewTab == crewName ? 'opacity-1' : 'opacity-[17.44%]'
-                )}
-              ></Link>
-            ))}
-          </div>
+        <img
+          src={formatPath(crewData.images.webp)}
+          alt={crewData.name}
+          className="mx-auto h-[223px] w-[327px] border-b-2 border-gray object-contain tablet:order-2 tablet:h-[532px] tablet:w-full tablet:border-b-0 desktop:h-auto"
+        />
+        <div className="mt-8 flex justify-center space-x-4 tablet:hidden">
+          {crewNames.map((crewName, index) => (
+            <Link
+              href={{ query: { crewTab: crewName } }}
+              key={index}
+              scroll={false}
+              className={cn(
+                'inline-block h-[10px] w-[10px] rounded-full bg-white transition-opacity hover:opacity-50',
+                crewTab == crewName ? 'opacity-1' : 'opacity-[17.44%]'
+              )}
+            ></Link>
+          ))}
         </div>
-        <div className="text-center tablet:order-1">
-          <h3 className="font-bellefair uppercase text-white opacity-[49.51%] tablet:text-2xl">
+        <div className="text-center tablet:order-1 desktop:self-center desktop:text-left">
+          <h3 className="font-bellefair uppercase text-white opacity-[49.51%] tablet:text-2xl desktop:text-[32px]">
             {crewData.role}
           </h3>
-          <h1 className="mt-2 font-bellefair text-2xl uppercase text-white tablet:mt-3 tablet:text-[40px]">
+          <h1 className="mt-2 font-bellefair text-2xl uppercase leading-normal text-white tablet:mt-3 tablet:text-[40px] desktop:text-[56px]">
             {crewData.name}
           </h1>
-          <p className="mt-4 text-[15px] leading-[25px] text-blue-100 tablet:text-base tablet:leading-[28px]">
+          <p className="mt-4 text-[15px] leading-[25px] text-blue-100 tablet:text-base tablet:leading-[28px] desktop:text-lg desktop:leading-[32px]">
             {crewData.bio}
           </p>
-          <div className="my-10 hidden justify-center space-x-4 tablet:flex ">
+          <div className="my-10 hidden justify-center space-x-4 tablet:flex desktop:mt-[120px] desktop:justify-start">
             {crewNames.map((crewName, index) => (
               <Link
                 href={{ query: { crewTab: crewName } }}
                 key={index}
                 scroll={false}
                 className={cn(
-                  'inline-block h-[10px] w-[10px] rounded-full bg-white transition-opacity hover:opacity-50',
+                  'inline-block h-[10px] w-[10px] rounded-full bg-white transition-opacity hover:opacity-50 desktop:h-[15px] desktop:w-[15px]',
                   crewTab == crewName ? 'opacity-1' : 'opacity-[17.44%]'
                 )}
               ></Link>
